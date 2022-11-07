1075c93d14803705aef023c17b5fcf398ab8fb02
abdul.ghafoor <abdul.ghafoor@followoz.com>
2022-11-07 16:37:35 +0500

    ok

---
+++ DevOps-7-C/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DevOps_7_C.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
